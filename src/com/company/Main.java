76e8a5e3692fc1255c716c901a6757df896961cb
Lukasz Gora <luke.gora@gmail.com>
2018-05-11 11:16:20 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+
+/*2. As warmup, write a ConsoleProgram that prints out the calls for a spaceship that is
+        about to launch. Write the numbers 10 down to 1 and then write liftoff. You must use
+        a loop:
+*/
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+        }
+        System.out.println("liftoff");
+    }
+}
